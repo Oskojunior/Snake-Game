b56524b58b554e912abd7243f78277bb1f2ca4c9
Michal Kuczynski <michal.kuczynski2000@wp.pl>
2021-11-14 19:48:11 +0100

    .

--- Interface.py
+++ Interface.py
@@ -9,7 +9,8 @@ grid_height = screen_height/gridsize
 
 class Interface:
     def __init__(self):
-        self.screen = pygame.display.set_mode((screen_width, screen_height), 0, 32)
+        self.screen = pygame.display.set_mode(
+            (screen_width, screen_height), 0, 32)
         self.surface = pygame.Surface(self.screen.get_size())
         self.surface = self.surface.convert()
         self.surface.fill((0, 0, 0))
@@ -33,11 +34,12 @@ class Interface:
         self.screen.blit(self.surface, (0, 0))
 
     def scoreboard(self, score):
-        text = self.myFont.render("Score {0}".format(score), True, (255, 255, 255))
+        text = self.myFont.render(
+            "Score {0}".format(score), True, (255, 255, 255))
         self.screen.blit(text, (5, 10))
 
     def draw_wall(self, positions):
-        if  not positions == (-1,-1):
+        if not positions == (-1, -1):
             for p in positions:
                 self.surface.blit(self.textWallHeight, p)
 
@@ -46,16 +48,20 @@ class Interface:
         text_options = self.myFont.render("Options", True, (255, 255, 255))
         indicator = self.myFont.render("->", True, (255, 255, 255))
         if state == 0:
-            self.screen.blit(indicator, ((screen_width / 2 - 40), (screen_height / 2 - 20)))
+            self.screen.blit(
+                indicator, ((screen_width / 2 - 40), (screen_height / 2 - 20)))
         elif state == -1:
-            self.screen.blit(indicator, ((screen_width / 2 - 40), (screen_height / 2)))
-        self.screen.blit(text_play, ((screen_width/2 - 20), (screen_height/2 -20)))
-        self.screen.blit(text_options, ((screen_width / 2 - 20), (screen_height / 2)))
+            self.screen.blit(
+                indicator, ((screen_width / 2 - 40), (screen_height / 2)))
+        self.screen.blit(
+            text_play, ((screen_width/2 - 20), (screen_height/2 - 20)))
+        self.screen.blit(
+            text_options, ((screen_width / 2 - 20), (screen_height / 2)))
 
     def pause(self):
         text_pause = self.myFont.render("Pause", True, (255, 255, 255))
-        self.screen.blit(text_pause, ((screen_width/2 - 20), (screen_height/2 -20)))
-
+        self.screen.blit(
+            text_pause, ((screen_width/2 - 20), (screen_height/2 - 20)))
 
     def difficulty(self, state):
         text_hard = self.myFont.render("Hard", True, (255, 255, 255))
@@ -63,20 +69,30 @@ class Interface:
         text_easy = self.myFont.render("Easy", True, (255, 255, 255))
         indicator = self.myFont.render("->", True, (255, 255, 255))
         if state == 0:
-            self.screen.blit(indicator, ((screen_width / 2 - 40), (screen_height / 2 - 20)))
+            self.screen.blit(
+                indicator, ((screen_width / 2 - 40), (screen_height / 2 - 20)))
         elif state == -1:
-            self.screen.blit(indicator, ((screen_width / 2 - 40), (screen_height / 2)))
+            self.screen.blit(
+                indicator, ((screen_width / 2 - 40), (screen_height / 2)))
         elif state == 1:
-            self.screen.blit(indicator, ((screen_width / 2 - 40), (screen_height / 2 - 40)))
-        self.screen.blit(text_medium, ((screen_width/2 - 20), (screen_height/2 -20)))
-        self.screen.blit(text_easy, ((screen_width / 2 - 20), (screen_height / 2)))
-        self.screen.blit(text_hard, ((screen_width / 2 - 20), (screen_height / 2 - 40)))
-
+            self.screen.blit(
+                indicator, ((screen_width / 2 - 40), (screen_height / 2 - 40)))
+        self.screen.blit(
+            text_medium, ((screen_width/2 - 20), (screen_height/2 - 20)))
+        self.screen.blit(
+            text_easy, ((screen_width / 2 - 20), (screen_height / 2)))
+        self.screen.blit(
+            text_hard, ((screen_width / 2 - 20), (screen_height / 2 - 40)))
 
     def game_over(self, counter):
         text_died = self.myFont.render("You DIED", True, (255, 255, 255))
-        text_menu = self.myFont.render("Press ESC to return to Main menu", True, (255, 255, 255))
-        text = self.myFont.render("You will respawn after {0} seconds".format(counter), True, (255, 255, 255))
-        self.screen.blit(text, ((screen_width/2 - 150), (screen_height/2 + 20)))
-        self.screen.blit(text_died, ((screen_width / 2 - 20), (screen_height / 2 - 20)))
-        self.screen.blit(text_menu, ((screen_width / 2 - 150), (screen_height / 2 + 100)))
+        text_menu = self.myFont.render(
+            "Press ESC to return to Main menu", True, (255, 255, 255))
+        text = self.myFont.render(
+            "You will respawn after {0} seconds".format(counter), True, (255, 255, 255))
+        self.screen.blit(
+            text, ((screen_width/2 - 150), (screen_height/2 + 20)))
+        self.screen.blit(
+            text_died, ((screen_width / 2 - 20), (screen_height / 2 - 20)))
+        self.screen.blit(text_menu, ((screen_width / 2 - 150),
+                         (screen_height / 2 + 100)))
