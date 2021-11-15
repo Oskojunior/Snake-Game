21b933d733b3ca63ee27a80f84d4a05a48e0abdd
Michal Kuczynski <michal.kuczynski2000@wp.pl>
2021-11-15 05:29:22 +0100

    interface finished

--- Interface.py
+++ Interface.py
@@ -1,4 +1,7 @@
 import pygame
+import pygame.freetype
+import os
+import sys
 screen_width = 480
 screen_height = 480
 
@@ -13,6 +16,13 @@ RED = (212, 36, 30)
 GREEN = (63, 242, 66)
 BLUE = (119, 247, 228)
 WHITE = (255, 255, 255)
+GREY = (34, 34, 34)
+GOLD = (255, 215, 0)
+GREENER_GREEN = (118, 255, 3)
+SUPER_RED = (225, 0, 0)
+font_path = os.path.join(sys.path[0], "PixelatedPusab.ttf")
+font_path2 = os.path.join(sys.path[0], "manaspc.ttf")
+font_path3 = os.path.join(sys.path[0], "ARCADE.TTF")
 
 
 class Interface:
@@ -28,8 +38,12 @@ class Interface:
         self.walls_color = 5
         self.food_color = 5
         self.surface.fill(self.backgroundcolors[self.background_color])
-        self.myFont = pygame.font.SysFont("monospace", 16)
+        self.myFont = pygame.font.Font(font_path, 32)
+        self.myFont2 = pygame.font.Font(font_path, 16)
+        self.myFont3 = pygame.font.Font(font_path, 24)
         self.font = pygame.font.SysFont("timesnewroman", 16)
+        self.justFont = pygame.font.Font(font_path2, 24)
+        self.titleFont = pygame.font.Font(font_path3, 64)
 
     def background(self):
         self.surface.fill(self.backgroundcolors[self.background_color])
@@ -49,8 +63,8 @@ class Interface:
         self.screen.blit(self.surface, (0, 0))
 
     def scoreboard(self, score):
-        text = self.myFont.render("Score {0}".format(score), True, WHITE)
-        self.screen.blit(text, (5, 10))
+        text = self.myFont3.render("Score {0}".format(score), True, GOLD)
+        self.screen.blit(text, (20, 20))
 
     def draw_wall(self, positions):
         textWallHeight = self.font.render(
@@ -60,88 +74,101 @@ class Interface:
                 self.surface.blit(textWallHeight, p)
 
     def menu(self, state):
-        text_play = self.myFont.render("Play", True, WHITE)
-        text_options = self.myFont.render("Options", True, WHITE)
-        indicator = self.myFont.render("->", True, WHITE)
+        title = self.titleFont.render("Snake", True, GREENER_GREEN)
+        text_play = self.myFont.render("Play", True, GREEN)
+        text_options = self.myFont.render("Options", True, PURPLE)
+        indicator = self.myFont2.render("->", True, WHITE)
+        border_bar = self.myFont.render(
+            "------------------------------------------------------------", True, GREENER_GREEN)
         if state == 0:
             self.screen.blit(
-                indicator, ((screen_width / 2 - 40), (screen_height / 2 - 20)))
+                indicator, ((screen_width / 2 - 60), (screen_height/2 - 25)))
         elif state == -1:
             self.screen.blit(
-                indicator, ((screen_width / 2 - 40), (screen_height / 2)))
+                indicator, ((screen_width / 2 - 60), (screen_height / 2 + 4)))
         self.screen.blit(
-            text_play, ((screen_width/2 - 20), (screen_height/2 - 20)))
+            text_play, (200, (screen_height/2 - 30)))
         self.screen.blit(
-            text_options, ((screen_width / 2 - 20), (screen_height / 2)))
+            text_options, (200, (screen_height / 2)))
+        self.screen.blit(title, (150, 10))
+        self.screen.blit(border_bar, (0, 60))
 
     def pause(self):
-        text_pause = self.myFont.render("Pause", True, WHITE)
+        text_pause = self.myFont.render("Pause ||", True, WHITE)
+        text_pause2 = self.myFont2.render("Press p to play", True, GREEN)
         self.screen.blit(
-            text_pause, ((screen_width/2 - 20), (screen_height/2 - 20)))
+            text_pause, (170, (screen_height/2 - 20)))
+        self.screen.blit(
+            text_pause2, (170, (screen_height/2 + 20)))
 
     def difficulty(self, state):
-        text_hard = self.myFont.render("Hard", True, WHITE)
-        text_medium = self.myFont.render("Medium", True, WHITE)
-        text_easy = self.myFont.render("Easy", True, WHITE)
-        indicator = self.myFont.render("->", True, WHITE)
+        text_hard = self.myFont.render("Hard", True, GREY)
+        text_medium = self.myFont.render("Medium", True, RED)
+        text_easy = self.myFont.render("Easy", True, GREEN)
+        indicator = self.myFont2.render("->", True, WHITE)
         if state == 0:
             self.screen.blit(
-                indicator, ((screen_width / 2 - 40), (screen_height / 2 - 20)))
+                indicator, ((screen_width / 2 - 60), (screen_height / 2 - 15)))
         elif state == -1:
             self.screen.blit(
-                indicator, ((screen_width / 2 - 40), (screen_height / 2)))
+                indicator, ((screen_width / 2 - 60), (screen_height / 2 + 15)))
         elif state == 1:
             self.screen.blit(
-                indicator, ((screen_width / 2 - 40), (screen_height / 2 - 40)))
+                indicator, ((screen_width / 2 - 60), (screen_height / 2 - 45)))
         self.screen.blit(
-            text_medium, ((screen_width/2 - 20), (screen_height/2 - 20)))
+            text_medium, (200, (screen_height/2 - 20)))
         self.screen.blit(
-            text_easy, ((screen_width / 2 - 20), (screen_height / 2)))
+            text_easy, (200, (screen_height / 2 + 10)))
         self.screen.blit(
-            text_hard, ((screen_width / 2 - 20), (screen_height / 2 - 40)))
+            text_hard, (200, (screen_height / 2 - 50)))
 
     def game_over(self, counter):
-        text_died = self.myFont.render("You DIED", True, WHITE)
-        text_menu = self.myFont.render(
-            "Press ESC to return to Main menu", True, WHITE)
-        text = self.myFont.render(
-            "You will respawn after {0} seconds".format(counter), True, WHITE)
+        text_died = self.myFont.render("You DIED", True, SUPER_RED)
+        text_menu = self.myFont2.render(
+            "Press ESC to return to Main menu", True, PURPLE)
+        text = self.myFont2.render(
+            "You will respawn after {0} seconds".format(counter), True, GREY)
         self.screen.blit(
             text, ((screen_width/2 - 150), (screen_height/2 + 20)))
         self.screen.blit(
-            text_died, ((screen_width / 2 - 20), (screen_height / 2 - 20)))
+            text_died, (170, (screen_height / 2 - 20)))
         self.screen.blit(text_menu, ((screen_width / 2 - 150),
                          (screen_height / 2 + 100)))
 
     def options(self, state):
-        text_snake = self.myFont.render(
+        description = self.myFont.render(
+            "Customize colors:", True, GREENER_GREEN)
+        text_snake = self.justFont.render(
             "Snake", True, self.itemcolors[self.snake_color])
-        text_food = self.myFont.render(
+        text_food = self.justFont.render(
             "Food", True, self.itemcolors[self.food_color])
-        text_walls = self.myFont.render(
+        text_walls = self.justFont.render(
             "Walls", True, self.itemcolors[self.walls_color])
-        text_background = self.myFont.render("Background", True, WHITE)
-        indicator = self.myFont.render("->", True, WHITE)
+        text_background = self.justFont.render("Background", True, WHITE)
+        indicator = self.myFont2.render("->", True, WHITE)
         if state == 0:
             self.screen.blit(
-                indicator, ((screen_width / 2 - 40), (screen_height / 2 - 20)))
+                indicator, ((screen_width / 2 - 85), (screen_height / 2 - 20)))
         elif state == -1:
             self.screen.blit(
-                indicator, ((screen_width / 2 - 40), (screen_height / 2)))
+                indicator, ((screen_width / 2 - 85), (screen_height / 2)))
         elif state == 1:
             self.screen.blit(
-                indicator, ((screen_width / 2 - 40), (screen_height / 2 - 40)))
+                indicator, ((screen_width / 2 - 85), (screen_height / 2 - 40)))
         elif state == -2:
             self.screen.blit(
-                indicator, ((screen_width / 2 - 40), (screen_height / 2 + 20)))
+                indicator, ((screen_width / 2 - 85), (screen_height / 2 + 20)))
         self.screen.blit(
-            text_food, ((screen_width/2 - 20), (screen_height/2 - 20)))
+            text_food, (170, (screen_height/2 - 20)))
         self.screen.blit(
-            text_walls, ((screen_width / 2 - 20), (screen_height / 2)))
+            text_walls, (170, (screen_height / 2)))
         self.screen.blit(
-            text_snake, ((screen_width / 2 - 20), (screen_height / 2 - 40)))
+            text_snake, (170, (screen_height / 2 - 40)))
         self.screen.blit(
-            text_background, ((screen_width / 2 - 20), (screen_height / 2 + 20)))
+            text_background, (170, (screen_height / 2 + 20)))
+        self.screen.blit(
+            description, (80, 30)
+        )
 
     def change_snake(self):
         if self.snake_color + 1 == len(self.itemcolors):
